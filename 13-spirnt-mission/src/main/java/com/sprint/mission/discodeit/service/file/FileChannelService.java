4a821b38f9b382a2e37d7c3532b53a44038685b6
rndud <rndud008@gmail.com>
2026-05-09 08:12:56 +0900

    feat: 채널 CRUD를 파일 CRUD로 대체

---
+++ 13-spirnt-mission/src/main/java/com/sprint/mission/discodeit/service/file/FileChannelService.java
@@ -0,0 +1,47 @@
+package com.sprint.mission.discodeit.service.file;
+
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.ChannelType;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
+import com.sprint.mission.discodeit.repository.file.FileChannelRepository;
+import com.sprint.mission.discodeit.service.ChannelService;
+
+import java.util.List;
+import java.util.UUID;
+
+public class FileChannelService implements ChannelService {
+
+   private final ChannelRepository channelRepository = new FileChannelRepository();
+   @Override
+   public void save(Channel channel) {
+      channelRepository.save(channel);
+   }
+
+   @Override
+   public Channel findById(UUID channelId) {
+      return channelRepository.findById(channelId);
+   }
+
+   @Override
+   public List<Channel> findAll() {
+      return channelRepository.findAll();
+   }
+
+   @Override
+   public void update(UUID channelId, String channelName, String description, ChannelType channelType) {
+      Channel byId = findById(channelId);
+      if(byId == null) return;
+
+      byId.update(channelName,description,channelType);
+
+      channelRepository.save(byId);
+   }
+
+   @Override
+   public void delete(UUID channelId) {
+      Channel byId = findById(channelId);
+      if(byId == null) return;
+
+      channelRepository.delete(byId.getId());
+   }
+}
