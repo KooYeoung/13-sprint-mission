9b26813f10442c2b656af8026f2e02961c4aa35c
rndud <rndud008@gmail.com>
2026-05-09 08:53:58 +0900

    feat: 메시지 JCF Repository 구현

--- 13-spirnt-mission/src/main/java/com/sprint/mission/discodeit/service/jcf/JCFMessageService.java
+++ 13-spirnt-mission/src/main/java/com/sprint/mission/discodeit/service/jcf/JCFMessageService.java
@@ -3,6 +3,12 @@ package com.sprint.mission.discodeit.service.jcf;
 import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.repository.jfc.JCFChannelRepository;
+import com.sprint.mission.discodeit.repository.jfc.JCFMessageRepository;
+import com.sprint.mission.discodeit.repository.jfc.JCFUserRepository;
 import com.sprint.mission.discodeit.service.ChannelService;
 import com.sprint.mission.discodeit.service.MessageService;
 import com.sprint.mission.discodeit.service.UserService;
@@ -13,7 +19,7 @@ import java.util.UUID;
 
 public class JCFMessageService implements MessageService {
 
-   private final static HashMap<UUID, Message> messageHashMap = new HashMap<>();
+   private final MessageRepository messageRepository = new JCFMessageRepository();
    private final UserService userService;
    private final ChannelService channelService;
 
@@ -21,44 +27,41 @@ public class JCFMessageService implements MessageService {
       this.userService = userService;
       this.channelService = channelService;
    }
-
    @Override
    public void save(Message message) {
       if(isUserOrChannelMissing(message.getUser().getId(), message.getChannel().getId())) return;
-
-      messageHashMap.put(message.getId(), message);
+      messageRepository.save(message);
    }
 
 
    @Override
    public Message findById(UUID messageId) {
 
-      return messageHashMap.get(messageId);
+      return messageRepository.findById(messageId);
    }
 
    @Override
    public List<Message> findAll() {
-      return messageHashMap.values().stream().toList();
+      return messageRepository.findAll();
    }
 
    @Override
    public void update(UUID messageId, String content) {
       Message byId = findById(messageId);
       if(byId == null) return;
-
       if(isUserOrChannelMissing(byId.getUser().getId(), byId.getChannel().getId())) return;
 
       byId.update(content);
+      messageRepository.save(byId);
    }
 
    @Override
    public void delete(UUID messageId) {
       Message byId = findById(messageId);
       if(byId == null) return;
-
       if(isUserOrChannelMissing(byId.getUser().getId(), byId.getChannel().getId())) return;
 
-      messageHashMap.remove(byId.getId());
+      messageRepository.delete(byId.getId());
    }
 
    private boolean isUserOrChannelMissing(UUID userId, UUID channelId) {
