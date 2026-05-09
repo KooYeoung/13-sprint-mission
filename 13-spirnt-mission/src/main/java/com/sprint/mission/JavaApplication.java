677509c811a85e0d4f7cd9512f2a7be1d16c5dbc
rndud <rndud008@gmail.com>
2026-05-09 09:17:20 +0900

    feat: 메시지 Basic Service 구현

--- 13-spirnt-mission/src/main/java/com/sprint/mission/JavaApplication.java
+++ 13-spirnt-mission/src/main/java/com/sprint/mission/JavaApplication.java
@@ -5,15 +5,19 @@ import com.sprint.mission.discodeit.entity.ChannelType;
 import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.repository.ChannelRepository;
+import com.sprint.mission.discodeit.repository.MessageRepository;
 import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.repository.file.FileChannelRepository;
+import com.sprint.mission.discodeit.repository.file.FileMessageRepository;
 import com.sprint.mission.discodeit.repository.file.FileUserRepository;
 import com.sprint.mission.discodeit.repository.jcf.JCFChannelRepository;
+import com.sprint.mission.discodeit.repository.jcf.JCFMessageRepository;
 import com.sprint.mission.discodeit.repository.jcf.JCFUserRepository;
 import com.sprint.mission.discodeit.service.ChannelService;
 import com.sprint.mission.discodeit.service.MessageService;
 import com.sprint.mission.discodeit.service.UserService;
 import com.sprint.mission.discodeit.service.basic.BasicChannelService;
+import com.sprint.mission.discodeit.service.basic.BasicMessageService;
 import com.sprint.mission.discodeit.service.basic.BasicUserService;
 import com.sprint.mission.discodeit.service.file.FileChannelService;
 import com.sprint.mission.discodeit.service.file.FileMessageService;
@@ -29,13 +33,15 @@ public class JavaApplication {
    public static void main(String[] args) {
       UserRepository jcfUserRepository = new JCFUserRepository();
       UserRepository fileUserRepository = new FileUserRepository();
-      UserService userService = new BasicUserService(jcfUserRepository);
+      UserService userService = new BasicUserService(fileUserRepository);
 
       ChannelRepository jcfChannelRepository = new JCFChannelRepository();
       ChannelRepository fileChannelRepository = new FileChannelRepository();
-      ChannelService channelService = new BasicChannelService(jcfChannelRepository);
-      
-      MessageService messageService = new JCFMessageService(userService, channelService);
+      ChannelService channelService = new BasicChannelService(fileChannelRepository);
+
+      MessageRepository jcfMessageRepository = new JCFMessageRepository();
+      MessageRepository fileMessageRepository = new FileMessageRepository();
+      MessageService messageService = new BasicMessageService(fileMessageRepository, fileUserRepository, fileChannelRepository);
 
       runUserServiceCrudTest(userService);
 
---
+++ 13-spirnt-mission/src/main/java/com/sprint/mission/discodeit/service/basic/BasicMessageService.java
@@ -0,0 +1,64 @@
+package com.sprint.mission.discodeit.service.basic;
+
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.service.MessageService;
+
+import java.util.List;
+import java.util.UUID;
+
+public class BasicMessageService implements MessageService {
+   private final MessageRepository messageRepository;
+   private final UserRepository userRepository;
+   private final ChannelRepository channelRepository;
+
+   public BasicMessageService(MessageRepository messageRepository, UserRepository userRepository, ChannelRepository channelRepository) {
+      this.messageRepository = messageRepository;
+      this.userRepository = userRepository;
+      this.channelRepository = channelRepository;
+   }
+
+   @Override
+   public void save(Message message) {
+      if(isUserOrChannelMissing(message)) return;
+      messageRepository.save(message);
+   }
+
+   @Override
+   public Message findById(UUID messageId) {
+      return messageRepository.findById(messageId);
+   }
+
+   @Override
+   public List<Message> findAll() {
+      return messageRepository.findAll();
+   }
+
+   @Override
+   public void update(UUID messageId, String content) {
+      Message message = findById(messageId);
+      if(message == null) return;
+      if(isUserOrChannelMissing(message)) return;
+      message.update(content);
+      messageRepository.save(message);
+   }
+
+   @Override
+   public void delete(UUID messageId) {
+      Message message = findById(messageId);
+      if(message == null) return;
+      if(isUserOrChannelMissing(message)) return;
+      messageRepository.delete(messageId);
+   }
+
+   private boolean isUserOrChannelMissing(Message message) {
+      User user = userRepository.findById(message.getUser().getId());
+      Channel byId = channelRepository.findById(message.getChannel().getId());
+      return user == null || byId == null;
+   }
+
+}
