6714fcc10691447806e4cb4122af106ef355c040
rndud <rndud008@gmail.com>
2026-05-09 09:06:41 +0900

    feat: 유저 Basic Service 구현

--- 13-spirnt-mission/src/main/java/com/sprint/mission/JavaApplication.java
+++ 13-spirnt-mission/src/main/java/com/sprint/mission/JavaApplication.java
@@ -4,9 +4,13 @@ import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.entity.ChannelType;
 import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.repository.file.FileUserRepository;
+import com.sprint.mission.discodeit.repository.jcf.JCFUserRepository;
 import com.sprint.mission.discodeit.service.ChannelService;
 import com.sprint.mission.discodeit.service.MessageService;
 import com.sprint.mission.discodeit.service.UserService;
+import com.sprint.mission.discodeit.service.basic.BasicUserService;
 import com.sprint.mission.discodeit.service.file.FileChannelService;
 import com.sprint.mission.discodeit.service.file.FileMessageService;
 import com.sprint.mission.discodeit.service.file.FileUserService;
@@ -19,7 +23,9 @@ import java.util.List;
 public class JavaApplication {
    
    public static void main(String[] args) {
-      UserService userService = new JCFUserService();
+      UserRepository jcfUserRepository = new JCFUserRepository();
+      UserRepository fileUserRepository = new FileUserRepository();
+      UserService userService = new BasicUserService(jcfUserRepository);
       ChannelService channelService = new JCFChannelService();
       MessageService messageService = new JCFMessageService(userService, channelService);
 
---
+++ 13-spirnt-mission/src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
@@ -0,0 +1,49 @@
+package com.sprint.mission.discodeit.service.basic;
+
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.service.UserService;
+
+import java.util.List;
+import java.util.UUID;
+
+public class BasicUserService implements UserService {
+   private final UserRepository userRepository;
+
+   public BasicUserService(UserRepository userRepository){
+      this.userRepository = userRepository;
+   }
+
+   @Override
+   public void save(User user) {
+      userRepository.save(user);
+   }
+
+   @Override
+   public User findById(UUID userId) {
+      return userRepository.findById(userId);
+   }
+
+   @Override
+   public List<User> findAll() {
+      return userRepository.findAll();
+   }
+
+   @Override
+   public void update(UUID userId, String nickname, String realName, String password, String email, String phoneNumber) {
+      User byId = findById(userId);
+      if (byId == null) return;
+
+      byId.update(nickname, realName, password, email, phoneNumber);
+
+      userRepository.save(byId);
+   }
+
+   @Override
+   public void delete(UUID userId) {
+      User byId = findById(userId);
+      if (byId == null) return;
+
+      userRepository.delete(byId.getId());
+   }
+}
