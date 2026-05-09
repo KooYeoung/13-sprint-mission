4c4904913cdd3dd5865e5bfd2c421329a6f2a674
rndud <rndud008@gmail.com>
2026-05-09 09:10:39 +0900

    feat: 채널 Basic Service 구현

--- 13-spirnt-mission/src/main/java/com/sprint/mission/JavaApplication.java
+++ 13-spirnt-mission/src/main/java/com/sprint/mission/JavaApplication.java
@@ -4,12 +4,16 @@ import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.entity.ChannelType;
 import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
 import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.repository.file.FileChannelRepository;
 import com.sprint.mission.discodeit.repository.file.FileUserRepository;
+import com.sprint.mission.discodeit.repository.jcf.JCFChannelRepository;
 import com.sprint.mission.discodeit.repository.jcf.JCFUserRepository;
 import com.sprint.mission.discodeit.service.ChannelService;
 import com.sprint.mission.discodeit.service.MessageService;
 import com.sprint.mission.discodeit.service.UserService;
+import com.sprint.mission.discodeit.service.basic.BasicChannelService;
 import com.sprint.mission.discodeit.service.basic.BasicUserService;
 import com.sprint.mission.discodeit.service.file.FileChannelService;
 import com.sprint.mission.discodeit.service.file.FileMessageService;
@@ -26,7 +30,11 @@ public class JavaApplication {
       UserRepository jcfUserRepository = new JCFUserRepository();
       UserRepository fileUserRepository = new FileUserRepository();
       UserService userService = new BasicUserService(jcfUserRepository);
-      ChannelService channelService = new JCFChannelService();
+
+      ChannelRepository jcfChannelRepository = new JCFChannelRepository();
+      ChannelRepository fileChannelRepository = new FileChannelRepository();
+      ChannelService channelService = new BasicChannelService(jcfChannelRepository);
+      
       MessageService messageService = new JCFMessageService(userService, channelService);
 
       runUserServiceCrudTest(userService);
---
+++ 13-spirnt-mission/src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
@@ -0,0 +1,49 @@
+package com.sprint.mission.discodeit.service.basic;
+
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.ChannelType;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
+import com.sprint.mission.discodeit.service.ChannelService;
+
+import java.util.List;
+import java.util.UUID;
+
+public class BasicChannelService implements ChannelService {
+   private final ChannelRepository channelRepository;
+   public BasicChannelService(ChannelRepository channelRepository) {
+      this.channelRepository = channelRepository;
+   }
+
+   @Override
+   public void save(Channel channel) {
+      channelRepository.save(channel);
+   }
+
+   @Override
+   public Channel findById(UUID channelId) {
+      return channelRepository.findById(channelId);
+   }
+
+   @Override
+   public List<Channel> findAll() {
+      return channelRepository.findAll();
+   }
+
+   @Override
+   public void update(UUID channelId, String channelName, String description, ChannelType channelType) {
+      Channel byId = findById(channelId);
+      if(byId == null) return;
+
+      byId.update(channelName,description,channelType);
+
+      channelRepository.save(byId);
+   }
+
+   @Override
+   public void delete(UUID channelId) {
+      Channel byId = findById(channelId);
+      if(byId == null) return;
+
+      channelRepository.delete(byId.getId());
+   }
+}
