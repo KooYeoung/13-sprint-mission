09fd496d9380145ac6038541abebab8b4d6cb001
rndud <rndud2208@gmail.com>
2026-06-22 15:56:46 +0900

    refactor: ChannelError 메시지 공통화

--- src/main/java/com/sprint/mission/discodeit/exception/ChannelNotFoundException.java
+++ src/main/java/com/sprint/mission/discodeit/exception/ChannelNotFoundException.java
@@ -2,6 +2,6 @@ package com.sprint.mission.discodeit.exception;
 
 public class ChannelNotFoundException extends CustomNotFoundException {
     public ChannelNotFoundException() {
-        super("존재하지 않는 채널 입니다.");
+        super(ChannelError.NOT_FOUND.getMessage());
     }
 }
