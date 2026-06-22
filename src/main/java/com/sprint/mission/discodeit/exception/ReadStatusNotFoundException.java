23f52e7cdfbe7a7fd0bd316a68c9ce3708296907
rndud <rndud2208@gmail.com>
2026-06-22 16:13:56 +0900

    refactor: ReadStatusError 메시지 공통화

--- src/main/java/com/sprint/mission/discodeit/exception/ReadStatusNotFoundException.java
+++ src/main/java/com/sprint/mission/discodeit/exception/ReadStatusNotFoundException.java
@@ -2,6 +2,6 @@ package com.sprint.mission.discodeit.exception;
 
 public class ReadStatusNotFoundException extends CustomNotFoundException {
     public ReadStatusNotFoundException() {
-        super("읽음 상태가 존재하지 않습니다.");
+        super(ReadStatusError.NOT_FOUND.getMessage());
     }
 }
