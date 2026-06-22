08cfa9a41f46a563d63c811533248ab511a54b8e
rndud <rndud2208@gmail.com>
2026-06-22 14:33:17 +0900

    refactor: class명 오타 및 문구 수정

--- src/main/java/com/sprint/mission/discodeit/exception/MessageNotFoundException.java
+++ src/main/java/com/sprint/mission/discodeit/exception/MessageNotFoundException.java
@@ -1,7 +1,7 @@
 package com.sprint.mission.discodeit.exception;
 
-public class MessageNotFountException extends CustomNotFoundException {
-    public MessageNotFountException() {
+public class MessageNotFoundException extends CustomNotFoundException {
+    public MessageNotFoundException() {
         super("존재하지 않는 메시지 입니다.");
     }
 }
