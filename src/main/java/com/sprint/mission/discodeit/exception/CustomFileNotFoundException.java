67703bd8107fa9b67dcc829ecc4ff1d234eb6963
rndud <rndud2208@gmail.com>
2026-06-27 01:19:51 +0900

    불필요한 파일 삭제

--- src/main/java/com/sprint/mission/discodeit/exception/CustomFileNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.sprint.mission.discodeit.exception;
-
-import com.sprint.mission.discodeit.exception.file.FileError;
-
-public class CustomFileNotFoundException extends CustomNotFoundException {
-    public CustomFileNotFoundException() {
-        super(FileError.NOT_FOUND.getMessage());
-    }
-}
