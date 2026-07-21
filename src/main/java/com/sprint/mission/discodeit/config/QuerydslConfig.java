070ff30c018fa1242a2cacfb712f232f4d4383e4
rndud <rndud008@gmail.com>
2026-07-21 11:15:35 +0900

    refactor(config): Querydsl 설정을 생성자 주입 방식으로 변경
      - EntityManager 필드 주입을 final 생성자 주입으로 변경
      - JPAQueryFactory Bean 메서드 포맷 정리

--- src/main/java/com/sprint/mission/discodeit/config/QuerydslConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/QuerydslConfig.java
@@ -2,18 +2,18 @@ package com.sprint.mission.discodeit.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class QuerydslConfig {
 
-    @PersistenceContext
-    private EntityManager entityManager;
+    private final EntityManager entityManager;
 
     @Bean
-    public JPAQueryFactory jpaQueryFactory(){
+    public JPAQueryFactory jpaQueryFactory() {
         return new JPAQueryFactory(entityManager);
     }
 }
