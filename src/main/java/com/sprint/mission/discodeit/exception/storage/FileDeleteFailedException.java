ad2a9ca5c4b18ca8ab3fb1cc7cc479af884d9193
rndud <rndud008@gmail.com>
2026-07-23 20:18:09 +0900

    fix: 파일 삭제 예외에 대상 경로 전달
    * FileDeleteFailedException 생성자에 Path 파라미터 추가
    * 삭제 실패 시 실제 파일 경로를 StorageException에 전달
    * 전역 예외 로그에서 삭제 대상 파일을 식별할 수 있도록 개선

--- src/main/java/com/sprint/mission/discodeit/exception/storage/FileDeleteFailedException.java
+++ src/main/java/com/sprint/mission/discodeit/exception/storage/FileDeleteFailedException.java
@@ -2,14 +2,15 @@ package com.sprint.mission.discodeit.exception.storage;
 
 import com.sprint.mission.discodeit.exception.ErrorCode;
 
+import java.nio.file.Path;
 import java.util.Map;
 
 public class FileDeleteFailedException extends StorageException {
-    public FileDeleteFailedException(Throwable cause) {
+    public FileDeleteFailedException(Path path, Throwable cause) {
         super(
                 ErrorCode.FILE_DELETE_FAILED,
                 Map.of(),
-                null,
+                path,
                 cause
         );
     }
