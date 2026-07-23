54e3dcbbb6423c56ca79f2a5867d688c9a42d655
rndud <rndud008@gmail.com>
2026-07-23 15:20:40 +0900

    fix: 사용자 수정 요청에서 null 허용 및 공백 값 검증

--- src/main/java/com/sprint/mission/discodeit/dto/request/user/UserUpdateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/request/user/UserUpdateRequest.java
@@ -4,12 +4,20 @@ import com.sprint.mission.discodeit.dto.command.user.UserUpdateCommand;
 import com.sprint.mission.discodeit.dto.request.ValidationMessage;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.With;
 
 @With
 public record UserUpdateRequest(
-        @NotBlank(message = ValidationMessage.USER_NAME)
+        @Pattern(
+                regexp = ".*\\S.*",
+                message = ValidationMessage.USER_NAME
+        )
         String newUsername,
+        @Pattern(
+                regexp = ".*\\S.*",
+                message = ValidationMessage.USER_PASSWORD
+        )
         String newPassword,
         @Email(message = ValidationMessage.USER_EMAIL)
         String newEmail
