74c4d6dd6e146f531c5c1f34abaa3108e7b6ab49
rndud <rndud008@gmail.com>
2026-07-24 16:29:19 +0900

    refactor: 메시지 리포지토리 중복 조회 메서드 제거

--- src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
@@ -1,15 +1,12 @@
 package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.Message;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.Instant;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -18,18 +15,6 @@ public interface MessageRepository extends JpaRepository<Message, UUID> , Messag
     @Override
     @EntityGraph(attributePaths = {"author", "channel", "author.userStatus", "author.profile"})
     Optional<Message> findById(UUID id);
-
-    @EntityGraph(attributePaths = {"author", "channel", "author.userStatus", "author.profile"})
-    Slice<Message> findAllByChannel_Id(UUID channelId, Pageable pageable);
-
-    @EntityGraph(attributePaths = {"author", "channel", "author.userStatus", "author.profile"})
-    @Query("""
-        select m
-        from Message m
-        where m.channel.id = :channelId
-            and m.createdAt < :cursor
-    """)
-    Slice<Message> findAllByChannelIdWithCursor(@Param("channelId") UUID channelId, Pageable pageable,@Param("cursor") Instant cursor);
 
     Optional<Message> findTop1ByChannel_IdOrderByCreatedAtDesc(UUID channelId);
 
